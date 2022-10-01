eb514f84ab4d64303916599f9a6105f15139969a
Jahnavi Yelamanchi <jahnaviyelamanchi03@gmail.com>
2022-10-01 20:12:21 +0530

    main files

---
+++ datacollection.py
@@ -0,0 +1,57 @@
+import time
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+
+
+cap = cv2.VideoCapture(0)
+
+detector= HandDetector(maxHands=1)
+
+offset = 20
+imgsize=300
+counter = 0
+folder = "data/Z"
+
+while True :
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x,y,w,h = hand['bbox']
+
+        imgwhite = np.ones((imgsize,imgsize,3),np.uint8)*255
+
+        imgcrop = img[y - offset : y + h +offset , x - offset : x + w + offset]
+
+        imgcropshape = imgcrop.shape
+
+        aspectratio = h/w
+
+        if aspectratio > 1:
+            k = imgsize/h
+            wcal = math.ceil(k*w)
+            imgresize = cv2.resize(imgcrop,(wcal,imgsize))
+            imgresizeshape = imgresize.shape
+            wgap = math.ceil((imgsize-wcal)/2)
+            imgwhite[:, wgap:wcal+wgap] = imgresize
+
+        else:
+            k = imgsize/w
+            hcal = math.ceil(k*h)
+            imgresize = cv2.resize(imgcrop,(imgsize,hcal))
+            imgresizeshape = imgresize.shape
+            hgap = math.ceil((imgsize-hcal)/2)
+            imgwhite[hgap:hcal + hgap, :] = imgresize
+
+        cv2.imshow("Imagecrop",imgcrop)
+        cv2.imshow("Imagewhite",imgwhite)
+
+    cv2.imshow("Image",img)
+    key = cv2.waitKey(1)
+
+    if key == ord("s"):
+        counter +=1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgwhite)
+        print(counter)
